5bbe276ea781e8d32de1a1bd0a49c7eece50a152
Ivan Castro <cluend91@gmail.com>
2021-11-17 13:45:27 -0500

    Agregando tabla en html

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+let nombreplanetas = ['Tierra', 'Marte']
+let distancia = [6, 3, '67']
+const tamanio = []
+
+let i = 0
+
+while (i <= nombreplanetas.length - 1) {
+   console.log(i);
+   console.log(nombreplanetas[i]);
+   i += 1
+}
+
+for (let i = 0; i < nombreplanetas.length; i++){
+    console.log(i);
+    console.log(nombreplanetas[i]);
+}
+
+nombreplanetas.map((planeta)=>{
+    console.log(planeta)
+})
+
+for (const key in Object) {
+    if (Object.hasOwnProperty.call(Object, key)) {
+        const element = Object[key];
+    }
+} 
+
+for (const key in nombreplanetas) {
+    console.log(nombreplanetas[key])
+}
+
+for(const iterator of Object){
+
+}
+
+//Recorrer Planeta
+for(const planeta of nombreplanetas) {
+    console.log(planeta)
+}
+
+//Operador de igualdad
+for(const key in distancia) {
+    if(distancia[key] == '5'){
+        console.log('se encontro la distancia')
+    }
+    else{
+        console.log('No se encotro la distancia')
+    }
+}
+
+for(const key in distancia) {
+    if(distancia[key] === '5'){
+        console.log('se encontro la distancia')
+    }
+    else{
+        console.log('No se encotro la distancia')
+    }
+}
